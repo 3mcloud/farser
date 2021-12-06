4d70e1098851e23553e234e9a720edf6e1242c6e
Mike Funaro <mfunaro@mmm.com>
2021-12-06 13:05:07 -0500

    WIP change to more flexible context.
    Rather than constrain the terminal nodes to lists and sets for the
    data and accumulator, use a single generic type to provide a "context"
    to the terminal nodes. Then the nodes can do as they see fit with the
    context and adds a lot more flexibility to what you can do inside and
    AST node.

--- src/main/java/com/mmm/his/cer/utility/farser/ast/node/operator/Or.java
+++ src/main/java/com/mmm/his/cer/utility/farser/ast/node/operator/Or.java
@@ -1,27 +1,25 @@
 package com.mmm.his.cer.utility.farser.ast.node.operator;
 
 import com.mmm.his.cer.utility.farser.ast.node.type.NonTerminal;
-import java.util.List;
-import java.util.Set;
 
 /**
  * Implementation of a non-terminal node for use in the AST. This class represents a logical OR
  * operation.
  *
- * @param <T> The type used in the terminal nodes.
+ * @param <C> The context type used in the terminal nodes.
  * @author Mike Funaro
  */
-public class Or<T> extends NonTerminal<T> {
+public class Or<C> extends NonTerminal<C> {
 
   @Override
-  public boolean evaluate(List<T> operands, Set<T> accumulator) {
-    boolean leftTrue = left.evaluate(operands, accumulator);
+  public boolean evaluate(C context) {
+    boolean leftTrue = left.evaluate(context);
 
     if (leftTrue) {
       return true;
     }
 
-    return right.evaluate(operands, accumulator);
+    return right.evaluate(context);
   }
 
   @Override
